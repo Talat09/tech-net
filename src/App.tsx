8c8babff51b7f4a207c407dc085071e000e24181
Talat09 <talat181008312@gmail.com>
2023-12-29 17:09:34 +0600

    project set up and tailwind set up done

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-500 text-4xl text-center text-white py-4 font-bold">
+        Tech Net
+      </h1>
+    </>
+  );
+}
+
+export default App;
