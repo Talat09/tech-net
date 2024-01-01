aa90bc2b98c2d598f4ab89861cd58821ea2891a6
Talat09 <talat181008312@gmail.com>
2024-01-01 21:25:48 +0600

    connect counterslice with store and use increment and decrement from home

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,5 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import { decrement, increment } from "../redux/features/Counter/counterSlice";
+
 const Home = () => {
-  return <div className="text-center text-4xl">Home</div>;
+  const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
+  return (
+    <div className="flex justify-center items-center gap-4">
+      <button
+        className="p-3 border-2 border-yellow-500 rounded-md mt-8"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+      <p className="text-green-500 text-3xl mt-8">Count: {count}</p>
+      <button
+        className="p-3 border-2 border-yellow-500 rounded-md mt-8"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+    </div>
+  );
 };
 
 export default Home;
